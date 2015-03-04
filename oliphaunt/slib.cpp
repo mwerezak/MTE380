938e4a26a48d5a60f5c9799e3ee6f504eff0c9cb
Nikhil Malhotra <nikhil.malhotra42@gmail.com>
2015-03-04 16:43:41 -0500

    Update slib and Coding standards
    I realized we can use the Docit plugin for Notepad++ to make
    documentation easier so I changed the coding standards to reflect that.
    The slib library is fully updated to reflect the new changes.

--- oliphaunt/slib.cpp
+++ oliphaunt/slib.cpp
@@ -1,37 +1,107 @@
 /*Sensor library
  *
- *Sound sensor read function*/
+ *By: Group 9
+ *2015
+ *This code the property of group 9 and cannot be used unless with
+ *the explicit permission of all of the group 9 members
+ *
+ *This library stores all the functions involved in retrieving data 
+ *from the sensors directly.
+ **/
 #include "slib.h"
- 
-long readSound(int _PIN) {
-  pinMode(_PIN, OUTPUT);
-  digitalWrite(_PIN, LOW);
-  delayMicroseconds(2);
-  digitalWrite(_PIN, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(_PIN, LOW);
-  pinMode(_PIN, INPUT);
-  return (pulseIn(_PIN, HIGH)/29/2);
-}
 
+/*
+ * Name         : readSound
+ *
+ * Synopsis     : long readSound(int _PIN)   *
+ * Arguments    : int  _PIN : Digital pin number of Sonar sensor
+ *
+ * Description  : returns distance value from Sonar sensor in cm
+ * 
+ * Returns      : long: distance in cm
+ */
+long readSound(int _PIN){
+    
+    pinMode(_PIN, OUTPUT);
+    digitalWrite(_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(_PIN, HIGH);
+    delayMicroseconds(5);
+    digitalWrite(_PIN, LOW);
+    pinMode(_PIN, INPUT);
+    return (pulseIn(_PIN, HIGH)/29/2);
+    
+}//end readSound
+
+/*
+ * Name         : readIRC
+ *
+ * Synopsis     : bool readIRC(int _PIN) *
+ * Arguments    : int  _PIN : Digital pin number of proximity IR sensor
+ *
+ * Description  : returns whether something is within 10 cm from the
+                  IR sensor
+ * 
+ * Returns      : bool:  1 no, 0 yes
+ */
 bool readIRC(int _PIN){
-  return digitalRead(_PIN);
-}
+    
+    return digitalRead(_PIN);
+    
+}//end readIRC
 
+/*
+ * Name         : startIRF
+ *
+ * Synopsis     : bool startIRF(int _PIN_ONE) *
+ * Arguments    : int  _PIN_ONE : Digital input pin of long distance IR sensor
+ *
+ * Description  : enables the IR sensor to start reading
+ * 
+ * Returns      : bool: 1-started, 0 failed to start
+ */
 bool startIRF(int _PIN_ONE){
-  pinMode(_PIN_ONE,OUTPUT);
-  digitalWrite(_PIN_ONE,HIGH);
-  return false;
-}
+    
+    pinMode(_PIN_ONE,OUTPUT);
+    digitalWrite(_PIN_ONE,HIGH);
+    return false;
 
-float readIRF(int _PIN_ONE, int _PIN_TWO){
-  delayMicroseconds(22);
-  int val = analogRead(_PIN_TWO);
-  delay(20);
-  return (float)exp((1/(val * 5.0/1024.0)+1.723)/0.909);
-}
+}//end startIRF
 
+/*
+ * Name         : readIRF
+ *
+ * Synopsis     : float readIRF(int _PIN_ONE, int _PIN_TWO) *
+ * Arguments    : int  _PIN_ONE : Analogue output pin of long
+                                  distance IR sensor
+ *
+ * Description  : returns distance from IR sensor in cm, Range 10-150 cm
+ * 
+ * Returns      : float: distnce in cm
+ */
+float readIRF(int _PIN_ONE){
+    
+    int val;
+    delayMicroseconds(22);
+    val = analogRead(_PIN_ONE);
+    delay(20);
+    return (float)exp((1/(val * 5.0/1024.0)+1.723)/0.909);
+
+}//end readIRF
+
+/*
+ * Name         : stopIRF
+ *
+ * Synopsis     : bool stopIRF(int _PIN_ONE) *
+ * Arguments    : int  _PIN_ONE : Digital input pin of long distance IR sensor
+ *
+ * Description  : stops IR sensor from reading data
+ * 
+ * Returns      : bool: 1-Stopped, 0-failed
+ */
 bool stopIRF(int _PIN_ONE){
-   digitalWrite(_PIN_ONE,LOW);
-  return true; 
-}
+
+    digitalWrite(_PIN_ONE,LOW);
+    return true; 
+
+}//end stopIRF
--- oliphaunt/slib.h
+++ oliphaunt/slib.h
@@ -1,4 +1,13 @@
-/*slib.h*/
+/*Sensor library
+ *
+ *By: Group 9
+ *2015
+ *This code the property of group 9 and cannot be used unless with
+ *the explicit permission of all of the group 9 members
+ *
+ *This library stores all the functions involved in retrieving data 
+ *from the sensors directly.
+ **/
 
 #include "arduino.h"
 
