e96e6785da3de8efca5304b07b2fcdd010cf426c
mwerezak <mwerezak@gmail.com>
2015-03-17 15:16:02 -0400

    Updates servo drive functions to take an argument from +1.0 to -1.0

--- oliphaunt/servolib.cpp
+++ oliphaunt/servolib.cpp
@@ -1,5 +1,6 @@
 #include "servolib.h"
 #include <Arduino.h>
+#include "utility.h"
 
 void driveServosStop() {
     analogWrite(SERVO_DRIVE_LEFT_PWM_PIN, SERVO_DRIVE_LEFT_PWM_STOP);
@@ -11,12 +12,34 @@ void driveServosNeutral() {
     analogWrite(SERVO_DRIVE_RIGHT_PWM_PIN, SERVO_DRIVE_RIGHT_PWM_NEUTRAL);
 }
 
-void driveServoLeft(char command) {
-    byte pwm = constrain(SERVO_DRIVE_LEFT_PWM_STOP + command, SERVO_DRIVE_LEFT_PWM_MIN, SERVO_DRIVE_LEFT_PWM_MAX);
+void driveServoLeftCmd(char command) {
+    byte pwm = constrain(command, SERVO_DRIVE_LEFT_PWM_MIN, SERVO_DRIVE_LEFT_PWM_MAX);
     analogWrite(SERVO_DRIVE_LEFT_PWM_PIN, pwm);
 }
 
-void driveServoRight(char command) {
-    byte pwm = constrain(SERVO_DRIVE_RIGHT_PWM_STOP + command, SERVO_DRIVE_RIGHT_PWM_MIN, SERVO_DRIVE_RIGHT_PWM_MAX);
+void driveServoRightCmd(char command) {
+    byte pwm = constrain(command, SERVO_DRIVE_RIGHT_PWM_MIN, SERVO_DRIVE_RIGHT_PWM_MAX);
     analogWrite(SERVO_DRIVE_RIGHT_PWM_PIN, pwm);
+}
+
+void driveServoLeft(float setting) {
+    char cmd = SERVO_DRIVE_LEFT_PWM_STOP;
+    if(setting > 0) {
+        cmd = char(LINSCALE(setting, 0.0, +1.0, SERVO_DRIVE_LEFT_PWM_STOP, SERVO_DRIVE_LEFT_PWM_MAX));
+    }
+    else if (setting < 0) {
+        cmd = char(LINSCALE(setting, -1.0, 0.0, SERVO_DRIVE_LEFT_PWM_MIN, SERVO_DRIVE_LEFT_PWM_STOP));
+    }
+    driveServoLeftCmd(cmd);
+}
+
+void driveServoRight(float setting) {
+    char cmd = SERVO_DRIVE_RIGHT_PWM_STOP;
+    if(setting > 0) {
+        cmd = char(LINSCALE(setting, 0.0, +1.0, SERVO_DRIVE_RIGHT_PWM_STOP, SERVO_DRIVE_RIGHT_PWM_MAX));
+    }
+    else if (setting < 0) {
+        cmd = char(LINSCALE(setting, -1.0, 0.0, SERVO_DRIVE_RIGHT_PWM_MIN, SERVO_DRIVE_RIGHT_PWM_STOP));
+    }
+    driveServoRightCmd(cmd);    
 }
--- oliphaunt/utility.h
+++ oliphaunt/utility.h
@@ -1,4 +1,6 @@
 
+#define LINSCALE(X, IN_MIN, IN_MAX, OUT_MIN, OUT_MAX) (X - IN_MIN)*(OUT_MAX - OUT_MIN)/(IN_MAX - IN_MIN) + OUT_MIN;
+
 template <typename T>
 static void printArray(T *array, int len, char *name) {
     Serial.print(name);
